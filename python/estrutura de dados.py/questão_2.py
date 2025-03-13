e28e39e79db5a6afba618fabc6741659ea0b1f30
Robert Batista <odfkgpokfgphfpodh@gmail.com>
2025-03-13 12:02:37 -0300

    Delete python directory

--- python/estrutura de dados.py/questão_2.py
+++
@@ -1,7 +0,0 @@
-nomes = []
-print('Digite 5 nomes')
-
-for i in range(5):
-    nomes.append(input(f'Digite o {i + 1}° nome: '))
-
-print(nomes)
--- python/pilharoda.py
+++
@@ -1,9 +0,0 @@
-import pilha
-l=pilha.pilha()
-for i in range(5):
-    l.empilhar()
-for x in range(5):
-    l.prin()
-    l.desempilha()
-    print("xxxxxxxxxxxxx")
-l.prin()
